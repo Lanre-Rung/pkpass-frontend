bedef8042728731f24a5cc56a5a047f507eef8c2
wyq <743897359@qq.com>
2024-08-04 15:10:04 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,18 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>
-        <div>
-          <h1>PASS DESIGNER</h1>
-        </div>
-      </el-header>
-      <el-container>
-        <el-aside>
-          <div class="nav">
-            <ul>
-              <li>MetaData</li>
-              <li>Image&Background</li>
-              <li>
-                <a href="#"
-                   v-on:click="toggleFieldsMenu">Fields</a>
-                <div v-show="isFieldsMenuVisible">
-                  <!-- 子菜单列表 -->
-                  <ul>
-                    <li>Field 1</li>
-                    <li>Field 2</li>
-                    <li>Field 3</li>
-                  </ul>
-                </div>
-              </li>
-              <li class="other-menu-item">Personalized Design</li>
-              <li class="other-menu-item">Trigger Mechanism</li>
-            </ul>
-          </div>
-        </el-aside>
-        <el-main>
-          <div class="main">
-            <!-- pass样例展示 -->
-            <div class="pass">
-              <Pass></Pass>
-            </div>
-            <!-- 1.基础数据 -->
-            <div class="data">
-              <MetaData></MetaData>
-            </div>
-          </div>
-        </el-main>
-      </el-container>
-      <el-footer>
-        <div>
-          <h1>底部</h1>
-        </div>
-      </el-footer>
-    </el-container>
-
+    <router-view></router-view>
   </div>
+
 </template>
 
 <script>
-import CreatePass from './components/CreatePass.vue';
-// import SlideNav from './components/SlideNav.vue';
-import MetaData from './components/MetaData.vue';
-import Pass from './components/Pass.vue';
+
 export default {
   name: 'App',
   components: {
-    CreatePass,
-    Pass,
-    MetaData
+
   },
-  data () {
-    return {
-      isFieldsMenuVisible: false
-    };
-  },
-  method: {
-    toggleFieldsMenu () {
-      this.isFieldsMenuVisible = !this.isFieldsMenuVisible;
-      if (this.isFieldsMenuVisible) {
-        // 获取子菜单的高度
-        this.adjustMenuPosition();
-      }
-    },
-    adjustMenuPosition () {
-      const fieldsItem = this.$refs.fields;
-      const submenuHeight = fieldsItem.querySelector('.submenu').offsetHeight;
-      // 计算Personalized Design和Trigger Mechanism的margin-top值
-      const marginTop = `${submenuHeight}px`;
-      this.$nextTick(() => {
-        const otherMenuItems = this.$el.querySelectorAll('.other-menu-item');
-        otherMenuItems.forEach(item => {
-          item.style.marginTop = marginTop;
-        });
-      });
-    }
-  }
+
 }
 </script>
 
@@ -117,34 +40,5 @@ a:active,
 a:focus {
   color: inherit; /* 保持颜色不变 */
   text-decoration: none; /* 去除悬停时的下划线 */
-}
-/* 页面样式 */
-.nav {
-  width: 200px;
-  height: 800px;
-  background-color: aquamarine;
-  overflow: hidden;
-}
-.nav ul {
-  padding: 0;
-}
-.nav li {
-  height: 60px;
-  border-bottom: 2px solid #fff;
-  line-height: 60px;
-  text-align: center;
-}
-/* 主体 */
-.main {
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-}
-.main .data {
-  margin-left: 250px;
-}
-.submenu {
-  padding-left: 20px;
-  background-color: #f9f9f9;
 }
 </style>
---
+++ src/pages/Setting.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="set">
+    <el-container>
+      <el-header>
+        <div>
+          <h1>PASS DESIGNER</h1>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px"
+                  style="background-color: rgb(238, 241, 246)">
+          <el-menu :default-openeds="['1', '3']">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-document"></i>MetaData</template>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><i class="el-icon-picture"></i>Image&BGC</template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1">Logo</el-menu-item>
+                <el-menu-item index="2-2">Icon</el-menu-item>
+                <el-menu-item index="2-2">Strip Image</el-menu-item>
+                <el-menu-item index="2-2">Footer Image</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="el-icon-setting"></i>Fields</template>
+              <el-menu-item-group>
+                <el-menu-item index="3-3">Header Fields</el-menu-item>
+                <el-menu-item index="3-1">Primary Fields</el-menu-item>
+                <el-menu-item index="3-2">Secondary Fields</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title"><i class="el-icon-user-solid"></i>Personalized Design</template>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title"><i class="el-icon-mobile"></i>Trigger Mechanism</template>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <div class="main">
+            <!-- pass样例展示 -->
+            <div class="pass">
+              <Pass></Pass>
+            </div>
+            <!-- 1.基础数据 -->
+            <div class="data">
+              <!-- <MetaData></MetaData> -->
+              <SelectImage>111</SelectImage>
+            </div>
+          </div>
+        </el-main>
+      </el-container>
+      <el-footer>
+        <div>
+          <h1>底部</h1>
+        </div>
+      </el-footer>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+import MetaData from '../components/MetaData.vue';
+import Pass from '../components/Pass.vue';
+import SelectImage from '../components/SelectImage.vue'
+export default {
+  name: 'Setting',
+  components: {
+    Pass,
+    MetaData,
+    SelectImage
+  },
+  data () {
+    return {
+      isFieldsMenuVisible: false
+    };
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+/* 页面样式 */
+/* 主体 */
+.main {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+}
+.main .data {
+  margin-left: 200px;
+}
+</style>
